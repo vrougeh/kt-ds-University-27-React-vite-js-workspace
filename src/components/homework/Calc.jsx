d8400f2d59cc2be6d0626298a3c4ef1c941b6055
vrougeh <dlaghdrbs907@gmail.com>
2026-04-24 11:18:29 +0900

    feat: feat homework code

--- src/components/homework/Calc.jsx
+++ src/components/homework/Calc.jsx
@@ -1,34 +1,88 @@
 import { useState } from "react";
 
 const Calc = () => {
-  const [first, setFirst] = useState(0);
-  const [second, setSecond] = useState(0);
-  const [result, setResult] = useState(0);
+  // const [first, setFirst] = useState(0);
+  // const [second, setSecond] = useState(0);
+  // const [result, setResult] = useState(0);
 
-  const plusHandler = () => {
-    setResult(first + Number(second));
-  };
+  const [{ first, second, result }, setNums] = useState({
+    first: 0,
+    second: 0,
+    result: 0,
+  });
 
-  const minusHandler = () => {
-    setResult(first - second);
-  };
+  // const plusHandler = () => {
+  //   setResult(first + Number(second));
+  // };
 
-  const multiHandler = () => {
-    setResult(first * second);
-  };
+  // const minusHandler = () => {
+  //   setResult(first - second);
+  // };
 
-  const divHandler = () => {
-    // second === 0 ? setResult("error") : setResult((first / second).toFixed(2));
+  // const multiHandler = () => {
+  //   setResult(first * second);
+  // };
 
-    if (second !== 0) {
-      const res = first / second;
-      setResult(res.toFixed(2));
-    } else {
-      setResult("Error");
+  // const divHandler = () => {
+  //   // second === 0 ? setResult("error") : setResult((first / second).toFixed(2));
+
+  //   if (second !== 0) {
+  //     const res = first / second;
+  //     setResult(res.toFixed(2));
+  //   } else {
+  //     setResult("Error");
+  //   }
+  // };
+
+  const onCalcButtonClickHander = (operator) => {
+    let result = 0;
+    if (operator === "+") {
+      result = first + Number(second);
+    } else if (operator === "-") {
+      result = first - second;
+    } else if (operator === "*") {
+      result = first * second;
+    } else if (operator === "/") {
+      result = first / second;
     }
+    setNums((prevNums) => {
+      return { ...prevNums, result };
+    });
+    // if (operator === "+") {
+    //   setNums((prevNums) => {
+    //     return { ...prevNums, result: first + Number(second) };
+    //   });
+    // } else if (operator === "-") {
+    //   setNums((prevNums) => {
+    //     return { ...prevNums, result: first - second };
+    //   });
+    // } else if (operator === "*") {
+    //   setNums((prevNums) => {
+    //     return { ...prevNums, result: first * second };
+    //   });
+    // } else if (operator === "/") {
+    //   setNums((prevNums) => {
+    //     return { ...prevNums, result: first / second };
+    //   });
+    // }
+
+    // if (operator === "+") {
+    //   setResult(first + Number(second));
+    // } else if (operator === "-") {
+    //   setResult(first - second);
+    // } else if (operator === "*") {
+    //   setResult(first * second);
+    // } else if (operator === "/") {
+    //   setResult(first / second);
+    // }
   };
+
   const firstHandler = (e) => {
-    setFirst(parseInt(e.target.value));
+    // setFirst(parseInt(e.target.value));
+    setNums((prevNums) => {
+      const newNums = { ...prevNums, first: parseInt(e.target.value) };
+      return newNums;
+    });
   };
 
   return (
@@ -36,16 +90,21 @@ const Calc = () => {
       <input type="number" value={first} onChange={firstHandler} />
 
       <div className="column">
-        <button onClick={plusHandler}>+</button>
-        <button onClick={minusHandler}>-</button>
-        <button onClick={multiHandler}>*</button>
-        <button onClick={divHandler}>/</button>
+        <button onClick={onCalcButtonClickHander.bind(this, "+")}>+</button>
+        <button onClick={onCalcButtonClickHander.bind(this, "-")}>-</button>
+        <button onClick={onCalcButtonClickHander.bind(this, "*")}>*</button>
+        <button onClick={onCalcButtonClickHander.bind(this, "/")}>/</button>
       </div>
 
       <input
         type="number"
         value={second}
-        onChange={(e) => setSecond(e.target.value)}
+        onChange={(e) => {
+          setNums((prevNums) => {
+            const newNums = { ...prevNums, second: parseInt(e.target.value) };
+            return newNums;
+          });
+        }}
       />
 
       <div className="result"> = {result}</div>
--- src/components/homework/Counter.jsx
+++ src/components/homework/Counter.jsx
@@ -2,24 +2,43 @@ import { useState } from "react";
 
 const Counter = () => {
   const [countValue, setCountValue] = useState(0);
-  const countIncreaseHandler = () => {
-    if (countValue < 100) {
-      setCountValue((prevCount) => {
+
+  const onButtonClickHandler = (event) => {
+    const className = event.target.classList.value;
+    const className2 = event.target.className;
+
+    console.dir(className);
+    console.dir(className2);
+
+    setCountValue((prevCount) => {
+      if (className.includes("decrease")) {
+        if (prevCount === 0) {
+          return prevCount;
+        }
+        return prevCount - 1;
+      } else if (className.includes("increase")) {
+        if (prevCount === 100) {
+          return prevCount;
+        }
         return prevCount + 1;
-      });
-    }
-  };
-  const countDecreaseHandler = () => {
-    if (countValue > 0) {
-      setCountValue(countValue - 1);
-    }
+      }
+      return prevCount;
+    });
   };
 
   return (
     <div className="homework">
-      <button onClick={countDecreaseHandler}>-</button>
+      <button
+        type="button"
+        className="decrease test"
+        onClick={onButtonClickHandler}
+      >
+        -
+      </button>
       <div className="countValue">{countValue}</div>
-      <button onClick={countIncreaseHandler}>+</button>
+      <button type="button" className="increase" onClick={onButtonClickHandler}>
+        +
+      </button>
     </div>
   );
 };
